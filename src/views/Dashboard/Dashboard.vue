776b41d31ff7e3fea36d60efbac05bbd3fc16d87
Victor Lourme <victor.lourme@icloud.com>
2020-09-20 11:41:50 +0200

    frontend(dashboard): use linechart for invoice per day

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -6,18 +6,7 @@
 					<CardIcon color="blue" icon="receipt" />Factures
 				</v-card-title>
 				<v-card-text>
-					<ApexCharts type="donut" :options="options" :series="series" />
-				</v-card-text>
-			</v-card>
-		</v-col>
-		<v-col cols="12" lg="4" md="4" sm="12">
-			<v-card class="mt-4">
-				<v-card-title>
-					<CardIcon color="teal accent-3" icon="check-bold" />Chiffre d'affaire
-				</v-card-title>
-				<v-card-text align="center" class="white--text">
-					<h1 class="display-1">{{ charts.prices.paid.value }} €</h1>
-					<span class="overline">+ {{ charts.prices.paid.tax }} € de T.V.A.</span>
+					<ApexCharts type="donut" height="350" :options="options" :series="series" />
 				</v-card-text>
 			</v-card>
 
@@ -25,11 +14,11 @@
 
 			<v-card class="mt-4">
 				<v-card-title>
-					<CardIcon color="amber accent-4" icon="clock" />En attente de paiement
+					<CardIcon color="teal accent-3" icon="check-bold" />Chiffre d'affaire
 				</v-card-title>
 				<v-card-text align="center" class="white--text">
-					<h1 class="display-1">{{ charts.prices.waiting.value }} €</h1>
-					<span class="overline">+ {{ charts.prices.waiting.tax }} € de T.V.A.</span>
+					<h1 class="display-1">{{ charts.prices.paid.value + charts.prices.waiting.value }} €</h1>
+					<span class="overline">+ {{ charts.prices.paid.tax + charts.prices.waiting.tax }} € de T.V.A.</span>
 				</v-card-text>
 			</v-card>
 
@@ -45,16 +34,41 @@
 				</v-card-text>
 			</v-card>
 		</v-col>
-		<v-col cols="12" lg="4" md="4" sm="12">
+		<v-col cols="12" lg="8" md="8" sm="12">
 			<v-card class="mt-4">
 				<v-card-title>
-					<CardIcon color="indigo" icon="account-group" />Clients
+					<CardIcon color="light-blue darken-4" icon="chart-line-variant" />Nombre de factures
 				</v-card-title>
-				<v-card-text align="center" class="white--text">
-					<h1 class="display-1">{{ charts.customers }}</h1>
-					<span class="overline">Clients enregistrés</span>
-				</v-card-text>
+
+				<ApexCharts type="line" height="350" :options="days.options" :series="days.series" />
 			</v-card>
+
+			<br />
+
+			<v-row>
+				<v-col cols="12" lg="6" md="6" sm="12">
+					<v-card class="mt-4">
+						<v-card-title>
+							<CardIcon color="amber accent-4" icon="clock" />Factures en attente
+						</v-card-title>
+						<v-card-text align="center" class="white--text">
+							<h1 class="display-1">{{ charts.prices.waiting.value }} €</h1>
+							<span class="overline">+ {{ charts.prices.waiting.tax }} € de T.V.A.</span>
+						</v-card-text>
+					</v-card>
+				</v-col>
+				<v-col cols="12" lg="6" md="6" sm="12">
+					<v-card class="mt-4">
+						<v-card-title>
+							<CardIcon color="indigo" icon="account-group" />Clients
+						</v-card-title>
+						<v-card-text align="center" class="white--text">
+							<h1 class="display-1">{{ charts.customers }}</h1>
+							<span class="overline">Clients enregistrés</span>
+						</v-card-text>
+					</v-card>
+				</v-col>
+			</v-row>
 		</v-col>
 	</v-row>
 </template>
@@ -72,6 +86,26 @@ export default {
 
 	data: () => ({
 		series: [],
+		days: {
+			options: {
+				stroke: {
+					curve: "smooth",
+				},
+				theme: {
+					mode: "dark",
+					palette: "palette3",
+				},
+				xaxis: {
+					categories: [],
+				},
+			},
+			series: [
+				{
+					name: "Nombre de factures",
+					data: [],
+				},
+			],
+		},
 		options: {
 			labels: ["Payé", "En attente", "Annulé"],
 			legend: {
@@ -104,18 +138,61 @@ export default {
 		...mapState("charts", ["charts"]),
 	},
 
+	watch: {
+		charts: {
+			deep: true,
+			immediate: true,
+			handler: "onLoad",
+		},
+	},
+
 	async mounted() {
 		try {
 			await this.$store.dispatch("charts/get");
-
-			this.series = [
-				this.charts.invoices.paid,
-				this.charts.invoices.waiting,
-				this.charts.invoices.canceled,
-			];
 		} catch (ex) {
 			console.log(ex);
 		}
+	},
+
+	methods: {
+		dateConverter(date) {
+			var dateParts = date.split("/");
+			var dateObject = new Date(
+				+dateParts[2],
+				dateParts[1] - 1,
+				+dateParts[0]
+			);
+			return dateObject;
+		},
+
+		onLoad(newValue) {
+			// Add series
+			this.series = [
+				newValue.invoices.paid,
+				newValue.invoices.waiting,
+				newValue.invoices.canceled,
+			];
+
+			// Sort dates
+			var ordered = {};
+			Object.keys(newValue.daily)
+				.sort((a, b) => {
+					return this.dateConverter(a) - this.dateConverter(b);
+				})
+				.forEach((key) => {
+					ordered[key] = newValue.daily[key];
+				});
+
+			// Delete series
+			this.days.options.xaxis.categories = [];
+			this.days.series[0].data = [];
+
+			// Set dates
+			for (let [key, value] of Object.entries(ordered)) {
+				this.days.options.xaxis.categories.push(key);
+				this.days.series[0].data.push(value);
+			}
+		},
 	},
 };
 </script>
